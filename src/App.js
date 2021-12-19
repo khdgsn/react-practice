f16ca8c5e1c6bcad357707c31d8848ca6f326464
Kathryn Hodgson <k3hdgsn@gmail.com>
2021-12-19 17:07:44 +0000

    working on .map()

--- src/App.js
+++ src/App.js
@@ -25,22 +25,43 @@ axios.request(options).then(function (response) {
 
 function App() {
   let arrOne = [{ 
-    name: "Kat", 
+    name: "K", 
     story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam.", 
     img: "https://pbs.twimg.com/profile_images/447374371917922304/P4BzupWu.jpeg"
   }];
   
   let arrTwo = [{ 
-    name: "Kath", 
+    name: "Kat", 
     story: "The Peacock's spectacular pattern of eyespots evolved to startle or confuse predators, make it one of the most easily recognized and best-known species.",
     img: "https://icatcare.org/app/uploads/2019/09/The-Kitten-Checklist-1.png"
   }];
 
   let arrThree = [{ 
-    name: "Kathryn", 
+    name: "Kath", 
     story: "The Belgian or Belgian Draft, French: Trait belge, Dutch: Belgisch Trekpaard, is a Belgian breed of draft horse.", 
     img: "https://i.pinimg.com/originals/cd/5b/e9/cd5be92c353be4ac383d4644ffeccb64.jpg"
   }];
+
+  // map over array to show new info
+  // create array
+  let arrFour = [
+     {name: "Kathryn"}, 
+     {story: "The Lionhead rabbit has a wool mane encircling the head, reminiscent of a male lion as its name implies."}, 
+     {img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Lionhead_rabbit_Dobby.jpg/330px-Lionhead_rabbit_Dobby.jpg"}
+     ];
+  
+  // map over
+  //let fourMap = arrFour.map(element => ({ value : element }) );
+  //console.log(fourMap);
+
+  /*
+  let numbers = [1, -1, 2, 3];
+  const items = numbers
+    .filter(n => n >= 0)
+    .map(n => ({ value : n }) );
+  
+  console.log(items);
+  */
 
   return (
     <div className="App">
@@ -67,6 +88,20 @@ function App() {
       <Content locator="two" author={arrTwo[0]["name"]} paragraph={arrTwo[0]["story"]} avatar={arrTwo[0]["img"]} />
 
       <Content locator="three" author={arrThree[0]["name"]} paragraph={arrThree[0]["story"]} avatar={arrThree[0]["img"]} />
+
+      <Content  />
+
+      <div locator="four" className="test">
+        {arrFour.map((item) => {
+          return(
+          <div key={item} className="arrayFour">
+            <p>{item.author}</p>
+            <p>{item.story}</p>
+            <p>{item.image}</p>
+          </div>
+          );
+        })}
+      </div>
 
       <header>
         <SocialFollow />
