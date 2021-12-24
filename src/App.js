3b5b178c1ce165d0b6b30674204bd3d9b93ea5e6
Kathryn Hodgson <k3hdgsn@gmail.com>
2021-12-24 08:50:21 +0000

    Clean up code

--- src/App.js
+++ src/App.js
@@ -24,34 +24,6 @@ axios.request(options).then(function (response) {
 */
 
 function App() {
-  /*
-  let arrOne = [{ 
-    name: "K", 
-    story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam.", 
-    img: "https://pbs.twimg.com/profile_images/447374371917922304/P4BzupWu.jpeg"
-  }];
-  
-  let arrTwo = [{ 
-    name: "Kat", 
-    story: "The Peacock's spectacular pattern of eyespots evolved to startle or confuse predators, make it one of the most easily recognized and best-known species.",
-    img: "https://icatcare.org/app/uploads/2019/09/The-Kitten-Checklist-1.png"
-  }];
-
-  let arrThree = [{ 
-    name: "Kath", 
-    story: "The Belgian or Belgian Draft, French: Trait belge, Dutch: Belgisch Trekpaard, is a Belgian breed of draft horse.", 
-    img: "https://i.pinimg.com/originals/cd/5b/e9/cd5be92c353be4ac383d4644ffeccb64.jpg"
-  }];
-
-  
-  let arrFour = [
-     {name: "Kathryn"}, 
-     {story: "The Lionhead rabbit has a wool mane encircling the head, reminiscent of a male lion as its name implies."}, 
-     {img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Lionhead_rabbit_Dobby.jpg/330px-Lionhead_rabbit_Dobby.jpg"}
-     ];
-  */
-     
-  
   let contArr = [
     { name: "K", 
       story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam.", 
@@ -93,14 +65,6 @@ function App() {
       <div className="welContainer">
         <h2>Hello and welcome to my blog</h2>
       </div>
-
-      {/* <Content locator="one" author={arrOne[0]["name"]} paragraph={arrOne[0]["story"]} avatar={arrOne[0]["img"]} />
-
-      <Content locator="two" author={arrTwo[0]["name"]} paragraph={arrTwo[0]["story"]} avatar={arrTwo[0]["img"]} />
-
-      <Content locator="three" author={arrThree[0]["name"]} paragraph={arrThree[0]["story"]} avatar={arrThree[0]["img"]} />
-
-      <Content  /> */}
 
       { <div locator="one" className="test">
         {contArr.map((item) => {
